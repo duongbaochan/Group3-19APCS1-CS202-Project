39abe9f41f36187dcc2dacbc79bf327ca6524947
pdqvinh19 <63652592+pdqvinh19@users.noreply.github.com>
2020-12-12 12:05:17 +0700

    Update function.h
    Move deadline to 16:00.

--- Decorator/Decorator Pattern/Decorator Pattern/function.h
+++ Decorator/Decorator Pattern/Decorator Pattern/function.h
@@ -6,26 +6,6 @@
 
 using namespace std;
 
-//Under Coding
-
 void menu();
 
-class PlainGrilledRiceCracker
-{
-protected:
-	int price;
-};
-
-class GrilledRiceCracker : public PlainGrilledRiceCracker
-{
-public:
-	void increase(int price);
-};
-
-class Topping
-{
-protected:
-	int price;
-public:
-	virtual void increase(int price);
-};
+// deadline 16h
