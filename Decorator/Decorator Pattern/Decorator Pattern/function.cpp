44def3f55a5a6bd44bd4646d2a8abeb19eea2408
pdqvinh19 <63652592+pdqvinh19@users.noreply.github.com>
2020-12-12 16:28:44 +0700

    Update Milk Tea

--- Decorator/Decorator Pattern/Decorator Pattern/function.cpp
+++ Decorator/Decorator Pattern/Decorator Pattern/function.cpp
@@ -1,90 +1,97 @@
 #include "function.h"
 
-//void menu()
-//{
-//	while (true)
-//	{
-//
-//	cout << "This is example of decorator pattern in real world: " << endl;
-//	cout << "Notifying info of the library to librarian user." << endl;
-//	cout << "1. Enter notification's content." << endl;
-//	cout << "2. Use sample notification." << endl;
-//	int c;
-//	cin >> c;
-//	string content = "";
-//	while (true)
-//	{
-//		switch (c)
-//		{
-//		case 1:
-//		{
-//			cin >> content;
-//			break;
-//		}
-//
-//		case 2:
-//		{
-//			content = "Your requested newspaper is available at shelf #15.";
-//			break;
-//		}
-//
-//		default:
-//		{
-//			cout << "Wrong choice. Please input again." << endl;
-//			break;
-//		}
-//		}
-//		if (c == 1 || c == 2)
-//			break;
-//	}
-//
-//	cout << "Please choose the type(s) of notification." << endl;
-//	cout << "1. Facebook," << endl;
-//	cout << "2. SMS," << endl;
-//	cout << "3. Slack." << endl;
-//	cout << "Enter \"a b\" if you want to choose multiple types." << endl;
-//	string tmp;
-//	getline(cin, tmp);
-//	string choice;
-//	getline(cin, choice);
-//
-//	stringstream ss;
-//	ss << choice;
-//
-//	int c1 = 0, c2 = 0, c3 = 0;
-//	ss >> c1 >> c2 >> c3;
-//
-//	Facebook fb(content);
-//	SMS sms(content);
-//	Slack slack(content);
-//
-//	Notifying fb_notify(&fb);
-//	Notifying sms_notify(&sms);
-//	Notifying slack_notify(&slack);
-//
-//	if (c1 == 1 || c2 == 1 || c3 == 1)
-//		fb_notify.notify();
-//	if (c1 == 2 || c2 == 2 || c3 == 2)
-//		sms_notify.notify();
-//	if (c1 == 3 || c2 == 3 || c3 == 3)
-//		slack_notify.notify();
-//
-//	char ch;
-//	while (true)
-//	{
-//		cout << "Replay the example Y/N?" << endl;
-//		cin >> ch;
-//		if (ch == 'N' || ch == 'n')
-//			break;
-//		else if (ch == 'Y' || ch == 'y')
-//			break;
-//		else
-//			cout << "Invalid choice. Please input again." << endl;
-//	}
-//
-//	if (ch == 'N' || ch == 'n')
-//		break;
-//	}
-//
-//}
+string MilkTea::serve()
+{
+    return 0;
+}
 
+int MilkTea::price()
+{
+    return 0;
+}
+
+// Concrete Component   
+string BaseMilkTea::serve()
+{
+    return "Milk Tea";
+}
+
+int BaseMilkTea::price()
+{
+    return 50000;
+}
+
+// Decorator  
+AddTopping::AddTopping(MilkTea* base_milk_tea) : milk_tea(base_milk_tea) {}
+
+string AddTopping::serve()
+{
+    return milk_tea->serve();
+}
+
+int AddTopping::price()
+{
+    return milk_tea->price();
+}
+
+
+
+// Concrete Decorator
+AddBubble::AddBubble(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
+
+string AddBubble::serve()
+{
+    return milk_tea->serve() + " + Bubble";
+}
+
+int AddBubble::price()
+{
+    return milk_tea->price() + 8000;
+}
+
+
+AddBlackSugar::AddBlackSugar(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
+
+string AddBlackSugar::serve()
+{
+    return milk_tea->serve() + " + Black Sugar";
+}
+
+int AddBlackSugar::price()
+{
+    return milk_tea->price() + 2000;
+}
+
+AddEggPudding::AddEggPudding(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
+
+string AddEggPudding::serve()
+{
+    return milk_tea->serve() + " + Egg Pudding";
+}
+
+int AddEggPudding::price()
+{
+    return milk_tea->price() + 10000;
+}
+
+AddFruitPudding::AddFruitPudding(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
+
+string AddFruitPudding::serve()
+{
+    return milk_tea->serve() + " + Fruit Pudding";
+}
+int AddFruitPudding::price()
+{
+    return milk_tea->price() + 11000;
+}
+
+AddWhiteBubble::AddWhiteBubble(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
+
+string AddWhiteBubble::serve()
+{
+    return milk_tea->serve() + " + White Bubble";
+}
+int AddWhiteBubble::price()
+{
+    return milk_tea->price() + 7000;
+}
--- Decorator/Decorator Pattern/Decorator Pattern/function.h
+++ Decorator/Decorator Pattern/Decorator Pattern/function.h
@@ -2,11 +2,8 @@
 
 #include <iostream>
 #include <string>
-//#include <sstream>
 
 using namespace std;
-
-//void menu();
 
 class MilkTea
 {
@@ -20,15 +17,8 @@ public:
 class BaseMilkTea : public MilkTea
 {
 public:
-    string serve()
-    {
-        return "Milk Tea";
-    }
-
-    int price()
-    {
-        return 50000;
-    }
+    string serve();
+    int price();
 };
 
 // Decorator  
@@ -37,18 +27,9 @@ class AddTopping : public MilkTea
 protected:
     MilkTea* milk_tea;
 public:
-
-    AddTopping(MilkTea* base_milk_tea) : milk_tea(base_milk_tea) {}
-
-    string serve()
-    {
-        return milk_tea->serve();
-    }
-
-    int price()
-    {
-        return milk_tea->price();
-    }
+    AddTopping(MilkTea* base_milk_tea);
+    string serve();
+    int price();
 };
 
 
@@ -56,75 +37,40 @@ public:
 class AddBubble : public AddTopping
 {
 public:
-    AddBubble(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
-
-    string serve()
-    {
-        return milk_tea->serve() + " + Bubble";
-    }
-    int price()
-    {
-        return milk_tea->price() + 8000;
-    }
+    AddBubble(MilkTea* base_milk_tea);
+    string serve();
+    int price();
 };
 
 
 class AddBlackSugar : public AddTopping
 {
 public:
-    AddBlackSugar(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
-
-    string serve()
-    {
-        return milk_tea->serve() + " + Black Sugar";
-    }
-    int price()
-    {
-        return milk_tea->price() + 2000;
-    }
+    AddBlackSugar(MilkTea* base_milk_tea);
+    string serve();
+    int price();
 };
 
 class AddEggPudding : public AddTopping
 {
 public:
-    AddEggPudding(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
-
-    string serve()
-    {
-        return milk_tea->serve() + " + Egg Pudding";
-    }
-    int price()
-    {
-        return milk_tea->price() + 10000;
-    }
+    AddEggPudding(MilkTea* base_milk_tea);
+    string serve();
+    int price();
 };
 
 class AddFruitPudding : public AddTopping
 {
 public:
-    AddFruitPudding(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
-
-    string serve()
-    {
-        return milk_tea->serve() + " + Fruit Pudding";
-    }
-    int price()
-    {
-        return milk_tea->price() + 11000;
-    }
+    AddFruitPudding(MilkTea* base_milk_tea);
+    string serve();
+    int price();
 };
 
 class AddWhiteBubble : public AddTopping
 {
 public:
-    AddWhiteBubble(MilkTea* base_milk_tea) : AddTopping(base_milk_tea) {}
-
-    string serve()
-    {
-        return milk_tea->serve() + " + White Bubble";
-    }
-    int price()
-    {
-        return milk_tea->price() + 7000;
-    }
+    AddWhiteBubble(MilkTea* base_milk_tea);
+    string serve();
+    int price();
 };
